3a801471b1aefdb8daa6530f1e3c93e80d17b3f0
Ezéchias W Kouda <ekouda006@gmail.com>
2021-06-04 15:30:35 +0000

    Ajout de fichier css, et des library importantantes pour le fonctionnement de l'application tel que Bootstrap, jQuery, etc... et refactoring du code en layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ asset('css/bootstrap-min.css') }}">
-    <title>Document</title>
-</head>
-<body>
-    <button class="btn btn-primary">TReste</button>
-</body>
-</html>
+@extends('layout/menu')
+
+@section('content')
+    @include('layout/dashboard')
+@endsection
